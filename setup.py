78d5d3198b1a207d46754e21d547ed8021fee0c0
Emily Ngahu <ngahuemily76@gmail.com>
2024-02-22 21:56:47 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup , find_packages
+
+setup(
+    name = 'MY PACKAGE',
+    version = '0.1',
+    license= 'MIT',
+    description= 'EDSA example pyton package',
+    long_description= open('README.md').read(),
+    install_requires = ['numpy'],
+    url = "",
+    author= 'Emily',
+    author_email= 'ngahuemily76@gmail.com'
+)
